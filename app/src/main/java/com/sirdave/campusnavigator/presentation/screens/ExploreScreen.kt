2b25e9d28036e61309e3c7dd107355247298cc5e
Abiola David <davidolanrewaju2000@gmail.com>
2024-03-07 09:27:37 +0100

    Explore screen --in progress

--- app/src/main/java/com/sirdave/campusnavigator/presentation/screens/ExploreScreen.kt
+++ app/src/main/java/com/sirdave/campusnavigator/presentation/screens/ExploreScreen.kt
@@ -1,8 +1,73 @@
 package com.sirdave.campusnavigator.presentation.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sirdave.campusnavigator.R
+import com.sirdave.campusnavigator.presentation.composables.PopularPlace
+import com.sirdave.campusnavigator.presentation.composables.SegmentedPlace
 
 @Composable
-fun ExploreScreen(){
+fun ExploreScreen(modifier: Modifier = Modifier){
+    Column(modifier = modifier.padding(horizontal = 8.dp)) {
+        Text(
+            text = stringResource(id = R.string.explore),
+            style = MaterialTheme.typography.titleLarge,
+            modifier = modifier.padding(8.dp),
+            fontWeight = FontWeight.Bold
+        )
 
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Search, contentDescription = null)
+            },
+            placeholder = {
+                Text(text = stringResource(id = R.string.search))
+            },
+            label = {
+                Text(text = stringResource(id = R.string.search))
+            },
+            modifier = modifier
+                .padding(horizontal = 8.dp, vertical = 4.dp)
+                .fillMaxWidth()
+        )
+
+        Spacer(modifier = modifier.width(8.dp))
+
+        Text(
+            text = stringResource(id = R.string.popular_places),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = modifier.padding(horizontal = 8.dp, vertical = 8.dp)
+        )
+
+        LazyRow(modifier = modifier){
+            items(5){
+                PopularPlace()
+            }
+        }
+
+        Spacer(modifier = modifier.width(16.dp))
+
+        SegmentedPlace(title = stringResource(id = R.string.halls_of_residence))
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ExplorePreview() {
+    ExploreScreen()
 }

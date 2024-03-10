5a1ee034461bef500dbf5f9cde90428470e32b72
Abiola David <davidolanrewaju2000@gmail.com>
2024-03-10 06:31:43 +0100

    Refactored image cards to load images asynchronously

--- app/src/main/java/com/sirdave/campusnavigator/presentation/composables/ImageCard.kt
+++ app/src/main/java/com/sirdave/campusnavigator/presentation/composables/ImageCard.kt
@@ -1,6 +1,5 @@
 package com.sirdave.campusnavigator.presentation.composables
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedCard
@@ -8,20 +7,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.sirdave.campusnavigator.R
+import com.sirdave.campusnavigator.domain.model.Place
 
 @Composable
-fun ImageCard(modifier: Modifier = Modifier){
+fun ImageCard(
+    place: Place,
+    modifier: Modifier = Modifier
+){
     OutlinedCard(
         modifier = modifier.height(IntrinsicSize.Min).width(IntrinsicSize.Min),
     ) {
         Box(modifier = modifier.fillMaxSize()){
-            Image(
-                painter = painterResource(id = R.drawable.ab3_stretching),
+            AsyncImage(
+                model = place.imageUrls[0],
+                placeholder = painterResource(id = R.drawable.ab3_stretching),
+                error = painterResource(id = R.drawable.ab3_stretching),
                 contentDescription = null,
+                modifier = modifier.height(150.dp).width(180.dp),
+                contentScale = ContentScale.Crop
             )
+
             Icon(
                 painter = painterResource(id = R.drawable.baseline_zoom_out_map),
                 contentDescription = null,
--- app/src/main/java/com/sirdave/campusnavigator/presentation/composables/ImageCardSmall.kt
+++ app/src/main/java/com/sirdave/campusnavigator/presentation/composables/ImageCardSmall.kt
@@ -1,7 +1,9 @@
 package com.sirdave.campusnavigator.presentation.composables
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.runtime.Composable
@@ -11,16 +13,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.sirdave.campusnavigator.R
+import com.sirdave.campusnavigator.domain.model.Place
 
 @Composable
-fun ImageCardSmall(modifier: Modifier = Modifier){
+fun ImageCardSmall(
+    place: Place,
+    modifier: Modifier = Modifier){
     OutlinedCard(
         modifier = modifier.size(85.dp),
     ) {
         Box(modifier = modifier.fillMaxSize()){
-            Image(
-                painter = painterResource(id = R.drawable.fc4_self_massage),
+            AsyncImage(
+                model = place.imageUrls[0],
+                placeholder = painterResource(id = R.drawable.fc4_self_massage),
+                error = painterResource(id = R.drawable.fc4_self_massage),
                 contentDescription = null,
                 contentScale = ContentScale.Crop
             )

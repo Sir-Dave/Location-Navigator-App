2c883da08828df24ca9a895962e3ac2f56d9dc3d
Abiola David <davidolanrewaju2000@gmail.com>
2024-03-12 12:36:17 +0100

    Set up retrofit for connecting to API endpoints

---
+++ app/src/main/java/com/sirdave/campusnavigator/data/remote/Api.kt
@@ -0,0 +1,29 @@
+package com.sirdave.campusnavigator.data.remote
+
+import com.sirdave.campusnavigator.data.remote.dto.response.PlaceDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface Api {
+
+    companion object{
+        const val BASE_URL = "https://location-navigator-backend-055a9239a7e2.herokuapp.com/"
+    }
+
+    @GET("api/v1/places")
+    suspend fun getPlacesByName(
+        @Query("name") name: String
+    ): Response<List<PlaceDto>>
+
+    @GET("api/v1/places")
+    suspend fun getPlacesByType(
+        @Query("email") type: String
+    ): Response<List<PlaceDto>>
+
+    @GET("api/v1/places")
+    suspend fun getAllPlaces(): Response<List<PlaceDto>>
+
+
+}
---
+++ app/src/main/java/com/sirdave/campusnavigator/data/remote/dto/response/PlaceDto.kt
@@ -0,0 +1,14 @@
+package com.sirdave.campusnavigator.data.remote.dto.response
+
+data class PlaceDto(
+    val id: Long,
+    var name: String,
+    var alias: String,
+    var longitude: Double,
+    var latitude: Double,
+    var placeType: String,
+    var category: String,
+    var imageUrls: List<String>,
+    var createdAt: String,
+    var updatedAt: String
+)

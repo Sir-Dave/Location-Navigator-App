67bc8e1ec28ea676c69a166440ef9fe2d6bdd92e
Abiola David <davidolanrewaju2000@gmail.com>
2024-03-13 13:12:31 +0100

    Implemented depedency injection

---
+++ app/src/main/java/com/sirdave/campusnavigator/di/AppModule.kt
@@ -0,0 +1,41 @@
+package com.sirdave.campusnavigator.di
+
+import com.sirdave.campusnavigator.data.remote.Api
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApi(okHttpClient: OkHttpClient): Api {
+        return Retrofit.Builder()
+            .baseUrl(Api.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+}
---
+++ app/src/main/java/com/sirdave/campusnavigator/di/RepositoryModule.kt
@@ -0,0 +1,26 @@
+package com.sirdave.campusnavigator.di
+
+import android.content.Context
+import com.sirdave.campusnavigator.data.PlaceRepositoryImpl
+import com.sirdave.campusnavigator.data.remote.Api
+import com.sirdave.campusnavigator.domain.repository.PlaceRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun providePlaceRepository(
+        @ApplicationContext context: Context,
+        api: Api
+    ): PlaceRepository {
+        return PlaceRepositoryImpl(context, api)
+    }
+}

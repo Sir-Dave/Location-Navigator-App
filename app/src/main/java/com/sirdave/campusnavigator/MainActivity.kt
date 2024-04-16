38c82646b2e359f4b286000e47b931b2f72b8d6c
Abiola David <davidolanrewaju2000@gmail.com>
2024-04-16 04:59:53 +0100

    Retrieve current location

--- app/src/main/java/com/sirdave/campusnavigator/MainActivity.kt
+++ app/src/main/java/com/sirdave/campusnavigator/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.sirdave.campusnavigator
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
@@ -10,12 +15,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.sirdave.campusnavigator.domain.model.BottomNavItem
 import com.sirdave.campusnavigator.presentation.composables.BottomNavigationBar
+import com.sirdave.campusnavigator.presentation.map.MapViewModel
 import com.sirdave.campusnavigator.presentation.navigation.Navigation
 import com.sirdave.campusnavigator.presentation.navigation.Screen
 import com.sirdave.campusnavigator.ui.theme.CampusNavigatorTheme
@@ -23,8 +33,32 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val viewModel: MapViewModel by viewModels()
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                viewModel.getDeviceLocation(fusedLocationProviderClient)
+            }
+        }
+
+    private fun askPermissions() = when (PackageManager.PERMISSION_GRANTED) {
+        ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) -> {
+            viewModel.getDeviceLocation(fusedLocationProviderClient)
+        }
+        else -> {
+            requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        askPermissions()
         val toBeShown = setOf(
             Screen.SearchScreen.route,
             Screen.ExploreScreen.route,
---
+++ app/src/main/java/com/sirdave/campusnavigator/presentation/map/MapState.kt
@@ -0,0 +1,7 @@
+package com.sirdave.campusnavigator.presentation.map
+
+import android.location.Location
+
+data class MapState(
+    val lastKnownLocation: Location? = null,
+)

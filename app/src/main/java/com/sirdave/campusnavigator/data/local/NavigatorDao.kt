bac5d0e56ea04233378364662bec365830fc2795
Abiola David <davidolanrewaju2000@gmail.com>
2024-05-03 15:35:49 +0100

    Fix search strategy in cache

--- app/src/main/java/com/sirdave/campusnavigator/data/local/NavigatorDao.kt
+++ app/src/main/java/com/sirdave/campusnavigator/data/local/NavigatorDao.kt
@@ -1,24 +1,32 @@
 package com.sirdave.campusnavigator.data.local
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
+@Dao
 interface NavigatorDao{
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertOnePlace(placeId: Long): PlaceEntity?
+    suspend fun insertOnePlace(place: PlaceEntity)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAllPlaces(): List<PlaceEntity>
+    suspend fun insertAllPlaces(places: List<PlaceEntity>)
 
     @Query("SELECT * FROM placeentity WHERE id = :placeId")
     suspend fun getOnePlace(placeId: Long): PlaceEntity?
+
+    @Query("SELECT * FROM placeentity WHERE name LIKE '%' || :name || '%'")
+    suspend fun getPlacesByName(name: String): List<PlaceEntity>
+
+    @Query("SELECT * FROM placeentity WHERE name LIKE '%' || :type || '%'")
+    suspend fun getPlacesByType(type: String): List<PlaceEntity>
 
     @Query("SELECT * FROM placeentity")
     suspend fun getAllPlaces(): List<PlaceEntity>
 
     @Query("DELETE FROM placeentity")
-    suspend fun clearAll()
+    suspend fun clearAllPlaces()
 
 }

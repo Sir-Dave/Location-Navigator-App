e9ee869e232ee3be0b6b1068f8c26e97d3fe1bf5
Abiola David <davidolanrewaju2000@gmail.com>
2024-05-03 14:16:29 +0100

    Updated mapper implementation

--- app/src/main/java/com/sirdave/campusnavigator/data/mapper/PlaceMapper.kt
+++ app/src/main/java/com/sirdave/campusnavigator/data/mapper/PlaceMapper.kt
@@ -1,10 +1,41 @@
 package com.sirdave.campusnavigator.data.mapper
 
+import com.sirdave.campusnavigator.data.local.PlaceEntity
 import com.sirdave.campusnavigator.data.remote.dto.response.PlaceDto
 import com.sirdave.campusnavigator.domain.model.Place
 import com.sirdave.campusnavigator.util.toLocalDateTime
 
 fun PlaceDto.toPlace(): Place{
+    return Place(
+        id = id,
+        name = name,
+        alias = alias,
+        longitude = longitude,
+        latitude = latitude,
+        placeType = placeType,
+        createdAt = createdAt.toLocalDateTime(),
+        imageUrls = imageUrls,
+        category = category,
+        updatedAt = updatedAt?.toLocalDateTime()
+    )
+}
+
+fun PlaceDto.toPlaceEntity(): PlaceEntity{
+    return PlaceEntity(
+        id = id,
+        name = name,
+        alias = alias,
+        longitude = longitude,
+        latitude = latitude,
+        placeType = placeType,
+        createdAt = createdAt,
+        imageUrls = imageUrls,
+        category = category,
+        updatedAt = updatedAt
+    )
+}
+
+fun PlaceEntity.toPlace(): Place{
     return Place(
         id = id,
         name = name,
